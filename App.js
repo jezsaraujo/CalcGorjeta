053c1fd7d9b8d8bfa59d6625a547cc4a732f836e
Jezsaraujo <jessika.mear@outlook.com>
2021-11-22 11:01:19 -0300

    colocando título

--- App.js
+++ App.js
@@ -2,5 +2,21 @@ import React, {useState} from 'react';
 import { Button, Text, View } from 'react-native';
 import styled  from 'styled-components/native';
 
+const Page = styled.SafeAreaView`
+flex:1;
+align-items:center;
+`;
+
+const HeaderText = styled.Text`
+  font-size:25px;
+`
+//arrow function
+export default()=> {
+  return(
+    <Page>
+        <HeaderText>Calculadora de Gorjeta</HeaderText>
+    </Page>
+  );
+}
 
 
