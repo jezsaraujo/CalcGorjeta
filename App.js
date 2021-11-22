d428f695710d16eaba8543f4da8f216aa6f34ea5
Jezsaraujo <jessika.mear@outlook.com>
2021-11-22 11:14:43 -0300

    colocando area do valor a gorjeta

--- App.js
+++ App.js
@@ -9,12 +9,27 @@ align-items:center;
 
 const HeaderText = styled.Text`
   font-size:25px;
-`
+`;
+
+const Input = styled.TextInput`
+  width:90%;
+  height:50px;
+  font-size:18px;
+  background-color: #eee;
+  margin-top:20px;
+  border-radius:10px;
+  padding:10px;
+`;
 //arrow function
 export default()=> {
   return(
     <Page>
         <HeaderText>Calculadora de Gorjeta</HeaderText>
+        <Input 
+        placeholder= "Quanto deu a conta?"
+        placeholderTextColor="#000"
+        keyboardType="numeric"
+        />
     </Page>
   );
 }
