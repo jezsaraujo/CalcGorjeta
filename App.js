4f408b14fbd5015dbdfdb552174732c9737f9802
Jezsaraujo <jessika.mear@outlook.com>
2021-11-29 11:22:58 -0300

    exercicio fadul teste

--- App.js
+++ App.js
@@ -1,57 +1,122 @@
-import React, {useState} from 'react';
-import { Button, Text, View } from 'react-native';
-import styled  from 'styled-components/native';
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, Alert } from 'react-native';
+import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
-flex:1;
-align-items:center;
+   flex: 1;
+   align-items:center;
+
 `;
 
 const HeaderText = styled.Text`
-  font-size:25px;
+   font-size:25px;
 `;
 
 const Input = styled.TextInput`
-  width:90%;
-  height:50px;
-  font-size:18px;
-  background-color: #eee;
-  margin-top:20px;
-  border-radius:10px;
-  padding:10px;
+   width: 90%;
+   height: 50px;
+   font-size:18px;
+   background-color:#EEE;
+   margin-top:20px;
+   border-radius:10px;
+   padding:10px;
 `;
 
 const CalcButton = styled.Button`
-  margin-top:10px;
+   margin-top:10px;
 `;
-//arrow function
-export default()=> {
-  const [bill, setBill] = useState(''); //iniciar com uma string vazia
-  const [tip, setTip] = useState(0);
 
-  const calc = ()=>{
-    let nBill = parseFloat(bill); //convertendo string pra float
+const ResultArea = styled.View`
+   width:100%;
+   margin-top:30px;
+   background-color:#EEE;
+   padding:20px;
+   justify-content:center;
+   align-items:center;
 
-    if(nBill){ // se verdadeiro -> se =1 ou >0
-      setTip((10/100) * nBill); //pega o valor de nBill e calc 10% e depois atualiza esse valor em setTip
-    }else{
-      alert("Digite o valor da conta")
-    }
-  }
+`;
+const ResultItemTitle = styled.Text`
+   font-size:18px;
+   font-weight:bold;
+`;
+const ResultItem = styled.Text`
+   font-size:15px;
+   margin-bottom:30px;
+`;
+const PctArea = styled.View`
+flex-direction:row;
+margin:20px;
+`;
+const PctItem = styled.Button``;
 
-  return(
-    <Page>
-        <HeaderText>Calculadora de Gorjeta</HeaderText>
-        <Input 
-        placeholder= "Quanto deu a conta?" 
-        placeholderTextColor="#000"
-        keyboardType="numeric" //teclado numerico
-        value={bill}
-        onChangeText={n=>setBill(n)}
-        />
-        <CalcButton title="Calcular" onPress={calc}/>
-    </Page>
-  );
+export default () => {
+   const [bill, setBill] = useState('');//state que vai conter o valor
+   const [bill2, setBill2] = useState('');//state que vai conter o valor
+   const [tip, setTip] = useState(0);// se iniciar com 0 será uma variavel numerica
+   const [pct, setPct] = useState(10); //gorjeta padrão
+   const[resultado, setResult] = useState(0);
+
+
+   const calc = () => {
+      let nBill = parseFloat(bill);
+
+      if (nBill) {
+         setTip((pct / 100) * nBill);
+      } 
+      setBill2(bill);
+   }
+
+   useEffect(()=>{
+     calc();
+   }, [pct]);
+
+   const result = () =>{
+      let resultado = calc();
+       = {(parseFloat(bill) + tip).toFixed(2)};
+   })
+
+   return (
+      <Page>
+         <HeaderText>Calculadora de Gorjeta</HeaderText>
+         <Input
+            placeholder="Quanto deu a conta?"
+            placeholderTextColor="#000"
+            keyboardType="numeric"
+            value={bill}
+            onChangeText={n => setBill(n)}//alterando o campo de entrada
+         />
+
+         <PctArea>
+            <PctItem title="5%" onPress={()=>setPct(5)}/>
+            <PctItem title="10%" onPress={()=>setPct(10)}/>
+            <PctItem title="15%" onPress={()=>setPct(15)}/>
+            <PctItem title="20%" onPress={()=>setPct(20)}/>
+         </PctArea>
+         <CalcButton title={`Calcular ${pct}%`} onPress={calc} />
+         
+         {tip > 0 &&
+
+            <ResultArea>
+               <ResultItemTitle>Valor da Conta</ResultItemTitle>
+               <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
+
+               <ResultItemTitle>Valor da Gorjeta</ResultItemTitle>
+               <ResultItem>R$ {tip.toFixed(2)} ({pct}%)</ResultItem>
+</ResultArea>
 }
+        
+               <ResultItemTitle> Valor Total</ResultItemTitle>
+               <ResultItem> R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+            
+         
+      </Page>
 
+   );
+}
+//criar de forma literal
+//function somar(x,y){
+ // return x + y;
+//}
 
+//chamando as funções
+//console.log(somar(10,5));
